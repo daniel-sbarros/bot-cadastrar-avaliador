265213eae1008fc888de14c34709beaa459479f1
Daniel Barros <daniel_barros@live.com>
2022-05-09 02:24:38 -0300

    Version 1.0.2

---
+++ BotCadastrarAvaliador/My.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotCadastrarAvaliador
+{
+    static class My
+    {
+        public static string ReplaceEspecialChars(string text)
+        {
+            string[] acentos   = new string[] { "ç", "Ç", "á", "é", "í", "ó", "ú", "ý", "Á", "É", "Í", "Ó", "Ú", "Ý", "à", "è", "ì", "ò", "ù", "À", "È", "Ì", "Ò", "Ù", "ã", "õ", "ñ", "ä", "ë", "ï", "ö", "ü", "ÿ", "Ä", "Ë", "Ï", "Ö", "Ü", "Ã", "Õ", "Ñ", "â", "ê", "î", "ô", "û", "Â", "Ê", "Î", "Ô", "Û" };
+            string[] semAcento = new string[] { "c", "C", "a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y", "a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "a", "o", "n", "a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "A", "O", "N", "a", "e", "i", "o", "u", "A", "E", "I", "O", "U" };
+            for (int i = 0; i < acentos.Length; i++) text = text.Replace(acentos[i], semAcento[i]);
+
+            return text.Trim();
+        }
+
+        public static string FormatNumber(int num)
+        {
+            return num < 10 ? $"0{num}" : $"{num}";
+        }
+    }
+}
